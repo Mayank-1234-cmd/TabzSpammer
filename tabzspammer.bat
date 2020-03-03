44f42a0d5e8bab0270ef641f9a4adf08562e5b17
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-03-03 18:29:41 -0500

    Update tabzspammer.bat

--- tabzspammer.bat
+++ tabzspammer.bat
@@ -1,19 +1,21 @@
 @echo off
-title mayank made a tab spammer
+color 0b
+:: Its my fav color scheme for cmd
+title tab spammer
 set lt=1
-echo tab spammer 
-:eyoy
+cls
+set /p tts=Tab to open a lot of times (ex. www.google.com or linustechtips.com): 
+:eyy
 cls
 
-start CHROME www.google.com
+start CHROME %tts%
 set lt=%lt%+1
 echo %lt% > tabzspammed.txt
 goto eyo
 :eyo
 cls
-echo tab spammer 
-cls
-start CHROME www.google.com
+
+start CHROME %tts%
 set lt=%lt%+1
 del "tabzspammed.txt"
 echo %lt% > tabzspammed.txt
