2dad95e68203d10f04299fa6cb4b25dd56dd41c2
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-03-08 19:27:55 -0400

    dont use

--- tabzspammer.bat
+++ tabzspammer.bat
@@ -6,8 +6,26 @@ set lt=1
 cls
 set /p tts=Tab to open a lot of times (ex. www.google.com or linustechtips.com): 
 se /p chc= Would you like to make a seperate file for this tab spamming? Y/N:
-IF "%var===chc%"=="" 
-:eyy
+IF "%chc%"=="Y" (
+echo The amount of tabs spammed and the file will be in the desktop.
+echo set var=1 > %tts%spammer.bat
+echo This tab spammer was made using TabzSpammer.bat > %tts%spammer.bat
+echo pause > %tts%spammer.bat
+echo @echo off > %tts%spammer.bat
+echo cls > %tts%spammer.bat
+echo :lt > %tts%spammer.bat
+echo del "tabzspammed.txt" > %tts%spammer.bat
+echo echo %
+echo start CHROME %tts% > %tts%spammer.bat
+echo set /a var=var+1  > %tts%spammer.bat
+echo echo var > tabzspammed.txt
+echo goto lt > %tts%spammer.bat
+echo 
+)
+if %chc%=="N" (
+goto hi
+)
+:hi
 cls
 
 start CHROME %tts%
