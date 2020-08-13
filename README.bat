c9ab0a49fdbc5f588cbfd7dd39c0a077b00c652e
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-08-12 22:43:59 -0400

    Update README.bat

--- README.bat
+++ README.bat
@@ -5,7 +5,7 @@ cls
 
 title TabzSpammer
 echo Borrowed some code for Readme.bat from https://stackoverflow.com/questions/33573277/randomizing-text-color-and-background-color-in-batch-file = specifically ByteHamster.
-echo TabzSpammer is a tabspammer. It makes a file called tabzspammer.txt which contains how many tabs were spammed. It asks you for what site to spam aswell.
+echo TabzSpammer is a tabspammer. It makes a file which says how many tabs were spammed
 : qwe
 set /a rand1=%random% %% 16
 set /a rand2=%random% %% 16
